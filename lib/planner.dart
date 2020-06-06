9f249dd6168d3055ea1cecd54485604a26db18f6
Floris Boom <florisboom@Floriss-Mac-mini.local>
2020-06-07 01:10:39 +0200

    Added form fields

--- lib/planner.dart
+++ lib/planner.dart
@@ -7,8 +7,62 @@ class Planner extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Planner')
-      )
+        child: PlannerForm(),
+      ),
+    );
+  }
+}
+
+class PlannerForm extends StatefulWidget {
+  @override
+  PlannerFormState createState() {
+    return PlannerFormState();
+  }
+}
+
+class PlannerFormState extends State<PlannerForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
